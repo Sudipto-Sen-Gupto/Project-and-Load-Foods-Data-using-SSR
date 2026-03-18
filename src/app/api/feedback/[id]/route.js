bd798b53414cc1c9c91e6d4e2acdab8c0518c060
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-03-18 16:06:28 +0600

    crud operation implement by using mongodb

--- src/app/api/feedback/[id]/route.js
+++ src/app/api/feedback/[id]/route.js
@@ -34,3 +34,26 @@ export async function DELETE(request,{params}){
          const result =await feedBack.deleteOne(query);
          return Response.json (result)
 }
+
+
+export async function  PATCH(request,{params}){
+         const {id}=await params;
+         const query={_id: new ObjectId(id)};
+         const {Food,message}=await request.json();
+         if(!message || typeof message!=="string" && !Food || typeof Food!=="string"){
+            return Response.json({
+               status:400,
+               message:"send a valid review and food name"
+            })
+         }
+
+         const updatedFeedback={
+            $set:{
+               message,
+               Food
+            }
+         }
+
+         const result=await feedBack.updateOne(query,updatedFeedback);
+         return Response.json(result)
+}
